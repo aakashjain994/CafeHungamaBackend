8d7481aaf09b1a1c3e554a2e280d4a16ee23dd31
mark-nakachon <harishchennupati54@gmail.com>
2019-08-02 19:54:39 +0530

    Made some changes in the bookings API

--- routes/user/bookings/get_all_list.js
+++ routes/user/bookings/get_all_list.js
@@ -2,6 +2,7 @@ const express = require("express"),
   router = express.Router({ mergeParams: true }),
   Booking = require("../../../models/booking");
   Ticket = require("../../../models/ticket");
+  Venue = require("../../../models/venue");
 /*router.get("/all/:id", (req, res) => {
    /*Booking.find({}, (err,allBookings)=> {
      //if they want only some data & not all data that can be done here
@@ -13,10 +14,27 @@ const express = require("express"),
 router.get('/:id',(req,res)=>{
 
   const userId = req.params.id;
+  const bookingdetails = [];
   console.log(userId);
-  Ticket.find({user:userId})
-        .then(bookings=>{
-          res.status(200).json(bookings);
+  Ticket.find({user:userId}).select('timeSlot ticketMRP status venueId -_id ')
+        .then(async bookings=>{
+          for(let i of bookings){
+          // const booking = await Booking.findOne({venueId:i.venueId})
+          //  bookings.findOne({venueId:i.venueId}).then(booking=>console.log(booking))
+
+          const venue = await Venue.findOne({_id:i.venueId}).select('venueName eventType');
+         // console.log(venue,i);
+          const bookingDetails = {}
+          //Object.assign() and destructuring are not working
+          bookingDetails['eventType'] = venue['eventType'];
+          bookingDetails['venueName'] = venue['venueName'];
+          bookingDetails['status'] = i['status'];
+          bookingDetails['timeSlot'] = i['timeSlot'];
+          bookingDetails['ticketMRP'] = i['ticketMRP'];
+          bookingdetails.push(bookingDetails);
+          }
+          //console.log(bookings);
+          res.status(200).json(bookingdetails);
         })
         .catch(err=>{
           console.log('error getting past bookings',err);
